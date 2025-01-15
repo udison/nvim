86ac35e7a1948acb33b96986b44c036d6f9e72ca
Roque Santos <roque-100@hotmail.com>
2025-01-15 19:35:38 -0300

    removed markdown linter due to bug

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,8 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- commented due to error
+        -- markdown = { 'markdownlint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
